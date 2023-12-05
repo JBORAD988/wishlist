b9e2d032e51aba32d04299e6856edde968980193
jayborad <co-20-081@rctiahmedabad.gujgov.edu.in>
2023-12-05 17:58:00 +0530

    update6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { WishItem } from "src/shared/models/wishItem";
-
+import { WishItem } from 'src/shared/models/wishItem';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   // items = [
@@ -15,56 +14,41 @@ export class AppComponent {
   //   new WishItem('complete the all task')
   // ];
 
-  items : WishItem[] = [
-      new WishItem('To Learn Angular'),
-      new WishItem('to go for walk',true),
-      new WishItem('find the grass cutter',true),
-      new WishItem('complete the all task')
-
+  items: WishItem[] = [
+    new WishItem('To Learn Angular'),
+    new WishItem('to go for walk', true),
+    new WishItem('find the grass cutter', true),
+    new WishItem('complete the all task'),
   ];
   title = 'whishlist';
   protected readonly WishItem = WishItem;
-
 
   // toggleItem(e:any){
   //   console.log(e);
   // }
 
 
+  newWishText = '';
 
-  toggleItem(items:WishItem){
-    items.isComplete = !items.isComplete;
-    console.log(items);
-  }
-  newWishText='';
+  listFilter: string = '0';
 
-  listFilter:string='0';
+  get visibleItems(): WishItem[] {
+    let value = this.listFilter;
 
-  visibleItems:WishItem[]= this.items;
-
-  addNewWish() {
-    this.items.push(new WishItem(this.newWishText));
-    this.newWishText='';
-  }
-  filterChanged(value: any) {
     if (value === '0') {
       // Show all items
-      this.visibleItems = this.items;
+      return this.items;
     } else if (value === '1') {
       // Show only incomplete items
-      this.visibleItems = this.items.filter(item => !item.isComplete);
+      return this.items.filter((item) => !item.isComplete);
     } else {
       // Show only completed items
-      this.visibleItems = this.items.filter(item => item.isComplete);
+      return this.items.filter((item) => item.isComplete);
     }
   }
 
-
-
+  addNewWish() {
+    this.items.push(new WishItem(this.newWishText));
+    this.newWishText = '';
   }
-
-
-
-
-
-
+}
